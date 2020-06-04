8725073794b19d8b6f9ea1893afb4e05ce68c4bd
Stan-Gogan <sjgogan@me.com>
2020-06-04 15:39:17 +1200

    draw canvas with rows and columns

---
+++ draw.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector(".canvas")
+const context = canvas.getContext("2d")
+const scale = 10;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
